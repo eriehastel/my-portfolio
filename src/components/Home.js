84bc63d0e9d160aad75ae76623db5816168612b0
erikoh <91188879+eriehastel@users.noreply.github.com>
2025-02-28 09:50:33 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -130,7 +130,7 @@ const Home = ({isOpen}) => {
                     src={ProfileImg} 
                     alt="Profile" 
                     className="w-full h-full rounded-full shadow-xl border-4 border-blue-600 dark:border-blue-400 bg-gray-500 dark:bg-gray-700"
-                      style={{width: "20px",objectFit: "cover"}}
+                      style={{height: "20px",objectFit: "cover"}}
                   />
                 </div>
               </div>
